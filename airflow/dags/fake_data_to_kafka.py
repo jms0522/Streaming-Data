27540f122335b9ec44d8d84071e1b908510df3b2
Ubuntu <ubuntu@ip-172-31-13-207.ap-northeast-2.compute.internal>
2024-08-27 18:44:07 +0000

    Update Airflow Dags

--- airflow/dags/fake_data_to_kafka.py
+++ airflow/dags/fake_data_to_kafka.py
@@ -2,27 +2,19 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 from modules.generate_fake_data import generate_fake_data
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
+from sqlalchemy import create_engine, Table, Column, Integer, MetaData
 from sqlalchemy.dialects.postgresql import JSONB
+from modules.send_to_kafka import send_to_kafka
+from airflow.exceptions import AirflowException
 
 # Postgres 연결 설정
 DATABASE_URI = 'postgresql+psycopg2://airflow:airflow@postgres/airflow'
-engine = create_engine(DATABASE_URI)
-metadata = MetaData()
-
-# 데이터 저장을 위한 테이블 정의
-fake_data_table = Table('fake_data', metadata,
-    Column('id', Integer, primary_key=True),
-    Column('data', JSONB)
-)
-
-metadata.create_all(engine)
 
 # 기본 DAG 설정
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 8, 1),
+    'start_date': datetime(2024, 8, 28),
     'retries': 1,
     'retry_delay': timedelta(minutes=5)
 }
@@ -37,11 +29,18 @@ dag = DAG(
 )
 
 # Task 1: Fake 데이터를 생성하고 Postgres에 저장하는 작업
-def task_generate_and_store_fake_data(**context):
-    data = generate_fake_data(30)
-    with engine.connect() as conn:
-        for user in data:
-            conn.execute(fake_data_table.insert().values(data=user))
+def task_generate_and_store_fake_data():
+    try:
+        data = generate_fake_data(30)
+        engine = create_engine(DATABASE_URI)
+        metadata = MetaData()
+        fake_data_table = Table('fake_data', metadata, autoload_with=engine)
+
+        with engine.connect() as conn:
+            for user in data:
+                conn.execute(fake_data_table.insert().values(data=user))
+    except Exception as e:
+        raise AirflowException(f"Error in task_generate_and_store_fake_data: {str(e)}")
 
 generate_data = PythonOperator(
     task_id='generate_and_store_data',
@@ -50,11 +49,18 @@ generate_data = PythonOperator(
 )
 
 # Task 2: Postgres에서 데이터를 읽어와 Kafka로 보내는 작업
-def task_send_to_kafka(**context):
-    with engine.connect() as conn:
-        result = conn.execute(fake_data_table.select())
-        for row in result:
-            send_to_kafka('fake-data', row['data'])
+def task_send_to_kafka():
+    try:
+        engine = create_engine(DATABASE_URI)
+        metadata = MetaData()
+        fake_data_table = Table('fake_data', metadata, autoload_with=engine)
+
+        with engine.connect() as conn:
+            result = conn.execute(fake_data_table.select())
+            for row in result:
+                send_to_kafka('fake-data', row['data'])
+    except Exception as e:
+        raise AirflowException(f"Error in task_send_to_kafka: {str(e)}")
 
 send_data = PythonOperator(
     task_id='send_data',
