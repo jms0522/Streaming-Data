b078aaf33e8a6bfe08fc55a8e92efd35f67fb401
Jangminsoo <wkdalstn8897@gmail.com>
2024-09-06 15:25:42 +0000

    Airflow Slack Connect

--- airflow/dags/data_generate_send_postgres_alert_slack.py
+++ airflow/dags/data_generate_send_postgres_alert_slack.py
@@ -1,19 +1,14 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
+from airflow.providers.slack.operators.slack import SlackAPIPostOperator
 from faker import Faker
 import shortuuid
 from datetime import datetime
 from airflow.utils.dates import days_ago
 import os
-from dotenv import load_dotenv
 
-load_dotenv(dotenv_path='/path/to/your/.env')
-
-# Slack 웹훅 URL을 환경 변수에서 가져오기
-SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL")
-
+# 가짜 데이터 생성
 def create_fake_user() -> dict:
     fake = Faker()
     fake_profile = fake.profile()
@@ -32,7 +27,7 @@ def create_fake_user() -> dict:
 
     return fake_dict
 
-def generate_fake_data(num_records: int) -> list:
+def generate_fake_data(num_records: int, **context) -> list:
     fake_users = []
     for _ in range(num_records):
         user = create_fake_user()
@@ -42,21 +37,21 @@ def generate_fake_data(num_records: int) -> list:
 # PostgreSQL에 데이터 삽입
 def insert_data_into_postgres(**context):
     fake_data = context['ti'].xcom_pull(task_ids='generate_fake_data')
-    postgres_hook = PostgresHook(postgres_conn_id='postgres_default')
+    postgres_hook = PostgresHook(postgres_conn_id='postgres_connector')
     conn = postgres_hook.get_conn()
     cursor = conn.cursor()
 
-    # Table 생성 쿼리 (필요한 경우 테이블 생성)
+    # 테이블 생성 쿼리
     create_table_query = """
-    CREATE TABLE IF NOT EXISTS fake_data (
+    CREATE TABLE IF NOT EXISTS new_fake_data (
         uuid VARCHAR(50),
         name VARCHAR(100),
         job VARCHAR(100),
         residence VARCHAR(255),
-        blood_group VARCHAR(5),
-        sex VARCHAR(10),
-        birthdate VARCHAR(10),
-        phone_number VARCHAR(20),
+        blood_group VARCHAR(30),
+        sex VARCHAR(20),
+        birthdate VARCHAR(30),
+        phone_number VARCHAR(100),
         email VARCHAR(100),
         timestamp TIMESTAMP
     );
@@ -66,7 +61,7 @@ def insert_data_into_postgres(**context):
 
     # 데이터 삽입 쿼리
     insert_query = """
-    INSERT INTO fake_data (uuid, name, job, residence, blood_group, sex, birthdate, phone_number, email, timestamp)
+    INSERT INTO new_fake_data (uuid, name, job, residence, blood_group, sex, birthdate, phone_number, email, timestamp)
     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
     """
 
@@ -81,7 +76,22 @@ def insert_data_into_postgres(**context):
     cursor.close()
     conn.close()
 
-# Slack 실패 알림 전송
+# 성공 시 Slack 알림 전송
+def send_slack_notification(**context):
+    completion_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    slack_msg = f"데이터 생성 후 데이터베이스 (Postgres)에 저장을 완료했습니다.\n완료 시간: {completion_time}"
+
+    slack_alert = SlackAPIPostOperator(
+        task_id='send_slack_notification',
+        slack_conn_id='slack_webhook',
+        text=slack_msg,
+        channel='#alerts',
+        username='airflow'
+    )
+
+    return slack_alert.execute(context=context)
+
+# 실패 시 Slack 알림 전송
 def task_failure_alert(context):
     task_instance = context.get('task_instance')
     dag_id = context.get('dag').dag_id
@@ -97,23 +107,25 @@ def task_failure_alert(context):
     *Log URL*: {log_url}
     """
 
-    slack_alert = SlackWebhookOperator(
+    slack_alert = SlackAPIPostOperator(
         task_id='slack_failure_notification',
-        webhook_token=SLACK_WEBHOOK_URL,
-        message=slack_msg,
+        slack_conn_id='slack_webhook',
+        text=slack_msg,
+        channel='#alerts',
         username='airflow'
     )
 
     return slack_alert.execute(context=context)
 
-# DAG 정의
+# DAG 기본 설정
 default_args = {
     'owner': 'airflow',
     'start_date': days_ago(1),
     'retries': 1,
-    'on_failure_callback': task_failure_alert  # 작업 실패 시 콜백
+    'on_failure_callback': task_failure_alert
 }
 
+# DAG 정의
 with DAG(
     dag_id='fake_data_pipeline',
     default_args=default_args,
@@ -121,25 +133,25 @@ with DAG(
     catchup=False,
 ) as dag:
 
-    # Task 1: 가짜 데이터 생성
+    # 가짜 데이터 생성 Task
     generate_fake_data_task = PythonOperator(
         task_id='generate_fake_data',
         python_callable=generate_fake_data,
-        op_args=[30],  # 30개의 데이터를 생성
+        op_args=[1000],  # 생성할 데이터 수 설정하기
         provide_context=True
     )
 
-    # Task 2: PostgreSQL에 데이터 삽입
+    # PostgreSQL에 데이터 삽입 Task
     insert_data_task = PythonOperator(
         task_id='insert_data_into_postgres',
         python_callable=insert_data_into_postgres,
         provide_context=True
     )
 
-    # Task 3: Slack 알림 전송 (성공 시)
+    # 성공 시 Slack 알림 Task
     slack_notification_task = PythonOperator(
         task_id='send_slack_notification',
-        python_callable=lambda: send_slack_notification(),
+        python_callable=send_slack_notification,
         provide_context=True
     )
 
